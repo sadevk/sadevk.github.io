117a38c3b5816d9e686e50a5766dfc428317e580
sadevk <sadev1214@gmail.com>
2023-11-22 15:01:39 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import NavBar from "./components/NavBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sadev Kaveesha",
+  title: "Sadev Kaveesha | Software Engineer",
   description: "Software Engineer",
 };
 
