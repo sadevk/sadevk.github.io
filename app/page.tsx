98a676aaa99cb458c5bdbece37e94c3e78d25a21
sadevk <sadev1214@gmail.com>
2024-08-04 09:05:48 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,167 +1,113 @@
-import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-screen">
-      <div className="w-full flex md:flex-row flex-col h-screen" id="home">
-        <div className="flex flex-col items-center justify-center w-full">
-          <iframe
-            src="https://lottie.host/?file=d5fee8e3-8309-40e6-878f-f4c7adf7fc90/GalFsvTskB.lottie"
-            className="md:w-[600px] md:h-[600px] w-96 h-96"
-          ></iframe>
-          <h1 className="md:text-3xl text-xl">
-            Hi! I&apos;m Sadev, Welcome to my Page!
-          </h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
       </div>
-      <div
-        className="w-full flex md:flex-row flex-col md:p-20 p-10 md:h-screen"
-        id="about"
-      >
-        <div className="md:w-1/3 p-10 flex justify-center items-center h-full">
-          <img
-            src="../assets/dp.jpg"
-            alt="Sadev Kaveesha(MySelf)"
-            className="rounded-3xl"
-          />
-        </div>
-        <div className="flex flex-col items-center justify-center gap-10 md:w-2/3 py-5">
-          <h1 className="md:text-6xl text-3xl md:mb-10">WHO AM I?</h1>
-          <p className="text-lg text-justify">
-            Hello! I&apos;m Sadev Kaveesha, a seasoned Software Developer with
-            two years of industry experience. I specialize in a diverse range of
-            technologies and possess strong organizational and management
-            skills.
-          </p>
-          <p className="text-lg text-justify">
-            With a passion for crafting innovative solutions, I have honed my
-            skills in various programming languages, including PHP, MySQL, HTML,
-            CSS, JAVA, C#, React Native, React JS, Next JS, and JavaScript. My
-            expertise extends across Mobile App Development, Web Application,
-            and Desktop Application Development.
-          </p>
-        </div>
+
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
-      <div
-        className="w-full flex md:flex-row flex-col md:p-20 p-10 md:h-screen"
-        id="services"
-      >
-        <div className="flex flex-col items-center justify-center gap-10 py-5">
-          <div className="w-full flex justify-center items-center">
-            <h1 className="md:text-6xl text-3xl md:mb-10">
-              WHAT SERVICES DO I OFFER?
-            </h1>
-          </div>
-          <div className="flex gap-5 md:flex-row flex-col">
-            <div className="md:w-1/3 flex flex-col gap-5 p-10 shadow shadow-white rounded-3xl hover:-translate-y-2 transition">
-              <h1 className="md:text-2xl text-xl">
-                Mobile Application Development
-              </h1>
-              <img src="../assets/mobile.png" alt="Mobile Development" />
-              <p className="text-justify">
-                Transform your ideas into functional and user-friendly mobile
-                applications. From concept to deployment, I bring your vision to
-                life on various platforms, including React Native, flutter and
-                native frameworks.
-              </p>
-            </div>
-            <div className="md:w-1/3 flex flex-col gap-5 p-10 shadow shadow-white rounded-3xl hover:-translate-y-2 transition">
-              <h1 className="md:text-2xl text-xl">Web Development</h1>
-              <img src="../assets/web.png" alt="Web Development" />
-              <p className="text-lg text-justify">
-                Create dynamic and engaging web applications tailored to your
-                specific needs. Utilizing the latest web technologies such as
-                NextJS, I design and develop responsive solutions that enhance
-                user experiences.
-              </p>
-            </div>
-            <div className="md:w-1/3 flex flex-col gap-5 p-10 shadow shadow-white rounded-3xl hover:-translate-y-2 transition">
-              <h1 className="md:text-2xl text-xl">
-                Desktop Application Development
-              </h1>
-              <img
-                src="../assets/pc.png"
-                alt="Desktop Application Development"
-              />
-              <p className="text-lg text-justify">
-                For projects requiring desktop solutions, I offer expertise in
-                crafting powerful and intuitive applications that meet the
-                unique requirements of your business or project for Windows,
-                Mac, Linux or any other platform.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="w-full flex md:flex-row flex-col md:p-20 p-10 md:h-screen">
-        <div className="md:w-1/3 p-10 flex justify-center items-center h-full">
-          <iframe
-            src="https://lottie.host/?file=e55e4b6f-e662-4a43-90c5-52591bcdf712/s8ZoznAIP1.lottie"
-            className="md:h-[600px] md:w-[600px] h-96 w-96"
-          ></iframe>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-10 md:w-2/3 py-5">
-          <h1 className="md:text-6xl text-3xl md:mb-10">
-            LET&apos;S COLLABORATE
-          </h1>
-          <p className="text-lg text-justify">
-            Whether you&apos;re a business looking to enhance your digital
-            presence or an entrepreneur with a groundbreaking idea, I am ready
-            to collaborate. Let&apos;s turn your concepts into reality through
-            strategic and innovative software development.
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Find in-depth information about Next.js features and API.
           </p>
-          <p className="text-lg text-justify">
-            Feel free to explore my portfolio to witness the range and quality
-            of projects I&apos;ve undertaken. If you&apos;re ready to embark on
-            a development journey, let&apos;s connect! I look forward to
-            bringing your ideas to life.
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
-          <h1 className="md:text-3xl text-xl md:my-10">REACH ME</h1>
-          <div className="flex md:flex-row flex-col gap-5" id="contact">
-            <div className="flex gap-5">
-              <a
-                href="http://wa.me/94775500325"
-                className="flex flex-col justify-center items-center gap-5 hover:no-underline border p-5 rounded-xl hover:font-bold w-44 h-32"
-              >
-                <img
-                  src="https://static.whatsapp.net/rsrc.php/v3/yz/r/ujTY9i_Jhs1.png"
-                  className="h-10 w-10"
-                />
-                <p>WhatsApp</p>
-              </a>
-              <a
-                href="https://www.facebook.com/sadevkaveesha.lk"
-                className="flex flex-col justify-center items-center gap-5 hover:no-underline border p-5 rounded-xl hover:font-bold w-44 h-32"
-              >
-                <img
-                  src="https://static.xx.fbcdn.net/rsrc.php/v3/y0/r/eFZD1KABzRA.png?_nc_eui2=AeGhclU00XYniIn_ujF1FbKhcQu7EDYlHPVxC7sQNiUc9S_cEyfvE0KvUhSFjOS_a6WDrXtWxg1HoenDCd6LjJXV"
-                  className="h-10 w-10"
-                />
-                <p>Facebook</p>
-              </a>
-            </div>
-            <div className="flex gap-5">
-              <a
-                href="tel:+94775500325"
-                className="flex flex-col justify-center items-center gap-5 hover:no-underline border p-5 rounded-xl hover:font-bold h-32 w-44"
-              >
-                <PhoneIcon className="h-10 w-10" />
-                <p>+94 77 5500 325</p>
-              </a>
-              <a
-                href="mailto:info@sadev.me"
-                className="flex flex-col justify-center items-center gap-5 hover:no-underline border p-5 rounded-xl hover:font-bold h-32 w-44"
-              >
-                <EnvelopeIcon className="h-10 w-10" />
-                <p>info@sadev.me</p>
-              </a>
-            </div>
-          </div>
-        </div>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Explore starter templates for Next.js.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
       </div>
-      <h1 className="my-5 text-gray-500 text-center">
-        Sadev Kaveesha &copy; {new Date().getFullYear()}
-      </h1>
     </main>
   );
 }
